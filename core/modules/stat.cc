d7114b5671348ebbef7faf6fdc25a135f4b8e0ea
Siddhant Gupta <dpsrkp.sid@gmail.com>
2021-12-07 02:05:47 +0000

    Added per flow rate calculation and threshold based burst detection.

--- core/modules/stat.cc
+++ core/modules/stat.cc
@@ -33,10 +33,14 @@
 #include <cstdlib>
 
 #include "../utils/format.h"
+#include "../utils/ether.h"
+#include "../utils/ip.h"
+#include "../utils/udp.h"
 
 #define DEFAULT_STAT_FREQUENCY 1
 #define SEC_IN_NS 1000000000
 #define HZ_TO_NS(freq) (SEC_IN_NS/freq)
+#define THRESHOLD 499900
 
 const Commands Stat::cmds = {
     {"set_frequency", "StatCommandSetFrequencyArg",
@@ -44,16 +48,8 @@ const Commands Stat::cmds = {
 
 
 CommandResponse Stat::Init(const bess::pb::StatArg &arg) {
-//  task_id_t tid;
   CommandResponse err;
   uint64_t freq = DEFAULT_STAT_FREQUENCY;
-
-/*
-  tid = RegisterTask(nullptr);
-  if (tid == INVALID_TASK_ID) {
-    return CommandFailure(ENOMEM, "Task creation failed");
-  }
-*/
 
   if (arg.frequency() != 0) {
       freq = arg.frequency();
@@ -75,36 +71,75 @@ CommandResponse Stat::GetInitialArg(const bess::pb::EmptyArg &) {
 }
 
 std::string Stat::GetDesc() const {
-  return bess::utils::Format("%lu", rate_);
+  return bess::utils::Format("%lu", frequency_);
+}
+
+Stat::FlowId Stat::GetId(bess::Packet *pkt) {
+  using bess::utils::Ethernet;
+  using bess::utils::Ipv4;
+  using bess::utils::Udp;
+
+  Ethernet *eth = pkt->head_data<Ethernet *>();
+  Ipv4 *ip = reinterpret_cast<Ipv4 *>(eth + 1);
+  size_t ip_bytes = ip->header_length << 2;
+  Udp *udp = reinterpret_cast<Udp *>(reinterpret_cast<uint8_t *>(ip) +
+                                     ip_bytes);  // Assumes a l-4 header
+  // TODO(joshua): handle packet fragmentation
+  FlowId id = {ip->src.value(), ip->dst.value(), udp->src_port.value(),
+               udp->dst_port.value(), ip->protocol};
+  return id;
+}
+
+void Stat::ComputeRate() {
+  uint64_t totalFlow = 0;
+  uint64_t maxFlowRate = 0;
+  Flow *maxFlow = nullptr;
+  for (auto ptr = flows_.begin(); ptr != flows_.end(); ptr++) {
+    Flow *f = ptr->second;
+    f->rate_ = f->count_ *(SEC_IN_NS/HZ_TO_NS(frequency_));
+    totalFlow += f->rate_;
+    f->count_ = 0;
+    if (f->rate_ > maxFlowRate){
+      maxFlowRate = f->rate_;
+      maxFlow = f;
+    }
+  }
+  lastTime_ = tsc_to_ns(rdtsc());
+  if (totalFlow>THRESHOLD) {
+    LOG(ERROR) << "Threshold exceeded. biggest flow rate: " << maxFlowRate;
+    LOG(ERROR) << "Total flow" << totalFlow;
+    LOG(ERROR) << "MaxFlow: " << maxFlow->id.src_ip;
+  }
+//  DebugPrint();
 }
 
 /* from upstream */
 void Stat::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
+  // insert packets in the batch into their corresponding flows
+  int cnt = batch->cnt();
+  for (int i = 0; i < cnt; i++) {
+    bess::Packet *pkt = batch->pkts()[i];
+
+    // TODO(joshua): Add support for fragmented packets.
+    FlowId id = GetId(pkt);
+    auto it = flows_.Find(id);
+
+    // if the Flow doesn't exist create one
+    // and add the packet to the new Flow
+    if (it == nullptr) {    
+      Flow *f = new Flow(id);
+      f->count_ = 1;
+      flows_.Insert(id, f);
+    } else {
+      it->second->count_ += 1;
+    }
+  }
   uint64_t now_ns = tsc_to_ns(rdtsc()); 
-  if (now_ns < lastTime_ + HZ_TO_NS(frequency_)) {
-    count_ += batch->cnt();
-  } else {
-    rate_ = count_ *(SEC_IN_NS/HZ_TO_NS(frequency_));
-    count_ = 0;
-    lastTime_ = tsc_to_ns(rdtsc());
+  if (now_ns >= lastTime_ + HZ_TO_NS(frequency_)) {
+    ComputeRate();
   }
   RunNextModule(ctx, batch);
 }
-
-/* to downstream */
-/*
-struct task_result Stat::RunTask(Context *ctx, bess::PacketBatch *batch,
-                                  void *) {
-
-  LOG(ERROR) << "Run Task Stat module";
-  RunNextModule(ctx, batch);
-
-  return {.block = false,
-          .packets = 0,
-          .bits = 0};
-}
-*/
-
 
 CommandResponse Stat::SetFrequency(uint64_t freq) {
   // TODO: Add any checks and validations.
@@ -115,6 +150,23 @@ CommandResponse Stat::SetFrequency(uint64_t freq) {
 CommandResponse Stat::CommandSetFrequency(
     const bess::pb::StatCommandSetFrequencyArg &arg) {
   return SetFrequency(arg.frequency());
+}
+
+void Stat::DebugPrint(){
+  uint64_t maxRate = 0;
+  Flow *maxFlow = nullptr;
+  for (auto ptr = flows_.begin(); ptr != flows_.end(); ptr++) {
+    Flow *f = ptr->second;
+    if (f->rate_>maxRate) {
+      maxRate = f->rate_;
+      maxFlow = f;
+    }
+  }
+  LOG(ERROR) << "MaxRate: " << maxRate ;
+  if (maxFlow != nullptr) {
+    LOG(ERROR) << "MaxFlow: " << maxFlow->id.src_ip;
+  }
+  LOG(ERROR) << "Count: " << flows_.Count();
 }
 
 ADD_MODULE(Stat, "stat",
--- core/modules/stat.h
+++ core/modules/stat.h
@@ -34,6 +34,10 @@
 #include "../module.h"
 #include "../pb/module_msg.pb.h"
 #include "../utils/time.h"
+#include "../utils/cuckoo_map.h"
+
+
+using bess::utils::CuckooMap;
 
 class Stat: public Module {
  public:
@@ -42,29 +46,73 @@ class Stat: public Module {
   Stat()
       : Module(),
         frequency_(),
-        lastTime_(),
-        count_(),
-        rate_() {
-//    is_task_ = false;
-//    propagate_workers_ = false;
+        lastTime_() {
     max_allowed_workers_ = Worker::kMaxWorkers;
   }
 
   CommandResponse Init(const bess::pb::StatArg &arg);
   CommandResponse GetInitialArg(const bess::pb::EmptyArg &);
 
-/*
-  struct task_result RunTask(Context *ctx, bess::PacketBatch *batch,
-                             void *arg) override;
-                             */
   void ProcessBatch(Context *ctx, bess::PacketBatch *batch) override;
 
   std::string GetDesc() const override;
 
   CommandResponse CommandSetFrequency(const bess::pb::StatCommandSetFrequencyArg &arg);
+  
 
  private:
+  // 5 tuple id to identify a flow from a packet header information.
+  struct FlowId {
+    uint32_t src_ip;
+    uint32_t dst_ip;
+    uint32_t src_port;
+    uint32_t dst_port;
+    uint8_t protocol;
+  };
+  
+  // stores the metrics of the flow.
+  struct Flow {
+    FlowId id;                  // allows the flow to remove itself from the map
+    uint64_t count_;
+    uint64_t rate_;
+    Flow() : id(), count_(), rate_() {};
+    Flow(FlowId new_id)
+        : id(new_id), count_(), rate_() {};
+  };
+  
+  // hashes a FlowId
+  struct Hash {
+    // a similar method to boost's hash_combine in order to combine hashes
+    inline void combine(std::size_t &hash, const unsigned int &val) const {
+      std::hash<unsigned int> hasher;
+      hash ^= hasher(val) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    }
+    bess::utils::HashResult operator()(const FlowId &id) const {
+      std::size_t hash = 0;
+      combine(hash, id.src_ip);
+      combine(hash, id.dst_ip);
+      combine(hash, id.src_port);
+      combine(hash, id.dst_port);
+      combine(hash, (uint32_t)id.protocol);
+      return hash;
+    }
+  };
+  struct EqualTo {
+    bool operator()(const FlowId &id1, const FlowId &id2) const {
+      bool ips = (id1.src_ip == id2.src_ip) && (id1.dst_ip == id2.dst_ip);
+      bool ports =
+          (id1.src_port == id2.src_port) && (id1.dst_port == id2.dst_port);
+      return (ips && ports) && (id1.protocol == id2.protocol);
+    }
+  };
+
+  //  Takes a Packet to get a flow id for. Returns the 5 element identifier for
+  //  the flow that the packet belongs to
+  FlowId GetId(bess::Packet *pkt);
+
+  void ComputeRate();
   CommandResponse SetFrequency(uint64_t size);
+  void DebugPrint() ;
 
   // Frequency at which the data sample needs to be collected.
   uint64_t frequency_;
@@ -72,12 +120,7 @@ class Stat: public Module {
   // Time for previous data point collection.
   uint64_t lastTime_;
 
-  // Total number of packets processed since previous data point collection.
-  uint64_t count_;
-
-  // Rate of packet arrival.
-  uint64_t rate_;
-
+  CuckooMap<FlowId, Flow *, Hash, EqualTo> flows_;
   bess::pb::StatArg init_arg_;
 
 };
